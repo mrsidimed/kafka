c9e077ac3881effa6b1cb7df50e4b6af796c9b39
Sidi Mohamed LAHWERTHI <mrsidimed@yahoo.fr>
2024-01-13 23:31:07 +0000

    last up to date

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Store the output of 'forever list' to a variable
+foreverList="info:    Forever processes running
+data:        uid  command       script              forever pid   id logfile                 uptime                   
+data:    [0] RX4N /usr/bin/node simpleProducerCG.js 31347   31361    /root/.forever/RX4N.log 0:0:3:53.376000000000005 
+data:    [1] 4GtQ /usr/bin/node producerMYSQL.js    31397   31410    /root/.forever/4GtQ.log 0:0:3:52.673             
+data:    [2] 0Nb6 /usr/bin/node consumerMYSQL.js    31685   31698    /root/.forever/0Nb6.log 0:0:0:41.246"
+
+# Use awk to extract PIDs from the 'forever list' output
+# Assumes that PIDs are in the second column of the output
+pids=$(echo "$foreverList" | awk 'NR>2 {print $6}')
+
+# Loop through each PID and stop the corresponding process
+for pid in $pids
+do
+    echo "$pid"
+done
